078fe91f5194cefb960334afef2a6c91c4c67a24
TL <113382633+5-le@users.noreply.github.com>
2022-11-25 14:33:02 -0500

    operators

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+	
+		
+		
+		
+		//Operators
+		
+		System.out.println("Enter a number");
+		
+		int first = in.nextInt();
 
+		int number = first;
+		
+		number = number -1;
+		number = number *3;
+		number = number + 12;
+		number = number/3;
+		number = number +5;
+		number = number - first;
+		
+		System.out.println(number);
+		
+		
+		in.close();
 	}
 
+	
 }
