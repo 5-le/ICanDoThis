e369542c1595e363fc7948f0e824c2189c401752
tl <113382633+5-le@users.noreply.github.com>
2022-12-04 21:12:04 -0500

    changes

--- src/Subprograms.java
+++ src/Subprograms.java
@@ -13,11 +13,26 @@ public class Subprograms {
 		
 		int n = one.length();
 		int o = two.length();
+		
+		int counter = 0;
 				
 		for (int i=0;i<n && i<o;i++) {
 			System.out.print(one.charAt(i));
 			System.out.print(two.charAt(i));
+			
+			
+			counter++;
+		}
 		
+		for (int j=counter;j<n || j <o;j++) {
+			
+			if (j<n) { System.out.print(one.charAt(j));
+			
+			}
+			else if (j<o) {System.out.print(two.charAt(j));
+	
+			}
+			else break;
 		}
 		
 		in.close();
